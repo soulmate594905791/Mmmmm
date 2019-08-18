dd7bae8b2ff6b9e3fcece4e0608c12efd5dc9584
soulmate594905791 <soulmate594905791@163.com>
2019-08-18 21:29:16 +0800

    连接

--- app/src/main/java/com/example/lenovo/mmmmm/MainActivity.java
+++ app/src/main/java/com/example/lenovo/mmmmm/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.lenovo.mmmmm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> mStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mStrings = new ArrayList<>();
+
+
+
     }
 
 }
