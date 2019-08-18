e6ab7f1fa439b83fb644f8af6eaca6e751348bf7
soulmate594905791 <soulmate594905791@163.com>
2019-08-18 21:26:09 +0800

    第一次传文件

---
+++ app/src/main/java/com/example/lenovo/mmmmm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lenovo.mmmmm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
